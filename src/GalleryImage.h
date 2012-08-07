335d2a1d0e1f5072ad81b3106b25966c4cca8e01
Katie Bartel <kwbartel@gmail.com>
2012-08-07 12:22:10 -0400

    fixed that for you

--- src/GalleryImage.h
+++ src/GalleryImage.h
@@ -14,6 +14,9 @@
 #include "cinder/gl/Texture.h"
 #include <string.h>
 #include "cinder/Timeline.h"
+#include "Vector.h"
+#include "cinder/Timeline.h"
+
 
 using namespace ci;
 using namespace ci::app;
@@ -26,19 +29,29 @@ class GalleryImage : public gl::Texture {
 public:
     Anim<float> x;
     Anim<float> y;
-    
+    Anim<float> a;
     Anim<float> scale;
+    Anim<float> rot;
     
-    float z;
+    Anim<float> z;
     float hOffset, vOffset;
     char align;
     gl::Texture tex;
+    gl::Texture back;
+    gl::Texture foreground;
     string path;
-    GalleryImage(string _path=NULL, char _align=4);
+    GalleryImage(string _path=NULL, char _align = CENTER);
     ~GalleryImage();
+    GalleryImage(Texture _tex,  char _align = CENTER);
+    GalleryImage(Texture front, Texture _back, char _align = CENTER);
+    
+    void rotate(int deg);
     void update();
     void draw();
     // void load(string _path);
+    
+private:
+    float scaleShadow();
     
     
 };
@@ -50,7 +63,7 @@ GalleryImage::GalleryImage(string _path, char _align){
     x = y = z;
     align = _align;
     path = _path;
-    // tex = gl::Texture(loadImage(loadUrl("http://25.media.tumblr.com/tumblr_m7c1drS9zB1qd0obwo1_500.jpg")));
+  //   tex = gl::Texture(loadImage(loadUrl("http://25.media.tumblr.com/tumblr_m7c1drS9zB1qd0obwo1_500.jpg")));
    tex = gl::Texture(loadImage(loadResource(path)));
     
     if(align<3){
@@ -67,22 +80,101 @@ GalleryImage::GalleryImage(string _path, char _align){
     } else {
         hOffset = -1*tex.getWidth();
     }
-    
+    foreground = loadImage(loadResource("postcard_foreground.png"));
 }
+
+GalleryImage::GalleryImage(Texture _tex,  char _align)
+{
+    scale = 1.0f;
+    x = y = z;
+    align = _align;
+    tex = _tex;
+    
+    if(align<3){
+        vOffset = 0;
+    } else if(align<6){
+        vOffset = -0.5f*tex.getHeight();
+    } else {
+        vOffset = -1*tex.getHeight();
+    }
+    if(align%3==0){
+        hOffset = 0;
+    } else if(align%3==1){
+        hOffset = -0.5f*tex.getWidth();
+    } else {
+        hOffset = -1*tex.getWidth();
+    }
+    foreground = loadImage(loadResource("postcard_foreground.png"));
+}
+
+GalleryImage::GalleryImage(Texture front, Texture _back, char _align)
+{
+    scale = 1.0f;
+    x = y = z;
+    align = _align;
+    tex = front;
+    back = _back;
+    
+    
+    if(align<3){
+        vOffset = 0;
+    } else if(align<6){
+        vOffset = -0.5f*tex.getHeight();
+    } else {
+        vOffset = -1*tex.getHeight();
+    }
+    if(align%3==0){
+        hOffset = 0;
+    } else if(align%3==1){
+        hOffset = -0.5f*tex.getWidth();
+    } else {
+        hOffset = -1*tex.getWidth();
+    }
+    foreground = loadImage(loadResource("postcard_foreground.png"));
+}
+
 void GalleryImage::update(){
     // nothing to see here
 }
 void GalleryImage::draw(){
-
-    gl::color(ColorA(255,255,255,0.5f));
+    float s = scaleShadow();
+   // console() << s << endl;
+    gl::color(ColorA(s, s, s, a));
     gl::pushMatrices();
-    gl::translate(x,y);
+    gl::translate(x,y,-z);
+    gl::enableAlphaBlending();
+    gl::rotate(Vec3f(0, rot, 0));
+    Rectf r = Rectf(0, 0, 500, 350);                                
+    //gl::translate(-tex.getWidth()/2.0, -tex.getHeight()/2.0);         // translate by r width Or tex width
+    gl::translate(-r.getWidth()/2.0, -tex.getHeight()/2.0);
 
-    // gl::color(ColorA((float)i/(float)numImages/2.0f+0.5f,0.75f,0.25f));
-    Rectf r = Rectf(-0.5f*tex.getWidth()*scale,-0.5f*tex.getHeight()*scale,scale*tex.getWidth(),scale*tex.getHeight());
-    gl::draw( tex, r);
+    gl::draw( tex, r );
+    if (back)  {
+        gl::pushMatrices();
+        gl::translate(0,0,-1);
+        gl::draw( back );
+        gl::popMatrices();
+    }
+    
+    gl::pushMatrices();
+    gl::translate(0,0,1);
+    gl::draw(foreground, r);
+    gl::popMatrices();
+    
     gl::popMatrices();
 
+}
+
+void GalleryImage::rotate(int deg)
+{
+//    rot += deg;
+
+    timeline().appendTo(&rot, rot + deg, 1.0f);
+}
+
+float GalleryImage::scaleShadow()
+{
+    return 1.0f - (z / 1000.0f * 1.0f);
 }
 
 #endif
